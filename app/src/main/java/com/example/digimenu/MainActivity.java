4c4918b9a4a3225d2499f99889d6e0f431f168a3
Soham Salkar <salkarsoham2@gmail.com>
2021-03-04 16:37:43 +0530

    Startpage ref in Main Act

--- app/src/main/java/com/example/digimenu/MainActivity.java
+++ app/src/main/java/com/example/digimenu/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.digimenu;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -19,9 +20,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
     }
+
+
+    new Handler().postDelayed(new Runnable(){
+        @Override
+                public void run(){
+            Intent intent = new Intent(packageContext: MainActivity.this,MainMenu.class);
+            startActivity(intent);
+            finish();
+
+
+
+    }.delayMillis: 3000  );
 
     }
 }
